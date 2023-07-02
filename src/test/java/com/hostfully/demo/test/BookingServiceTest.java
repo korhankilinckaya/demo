2406ef7446cefa4f587a918835ee967331e04a8d
Korhan <korhan.kilinckaya@insightsoftware.com>
2023-07-03 01:30:14 +0300

    first implementations

--- src/test/java/com/hostfully/demo/test/BookingServiceTest.java
+++ src/test/java/com/hostfully/demo/test/BookingServiceTest.java
@@ -1,8 +1,11 @@
 package com.hostfully.demo.test;
 
+import com.hostfully.demo.exceptions.BookingValidationException;
 import com.hostfully.demo.model.Booking;
 import com.hostfully.demo.repository.BookingRepository;
 import com.hostfully.demo.service.BookingService;
+import com.hostfully.demo.service.BookingValidationService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -15,6 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class BookingServiceTest {
@@ -23,69 +27,130 @@ public class BookingServiceTest {
   private BookingRepository bookingRepository;
 
   @InjectMocks
+  private BookingValidationService bookingValidationService;
+
+  @InjectMocks
   private BookingService bookingService;
 
   @BeforeEach
   public void setup() {
-    MockitoAnnotations.initMocks(this);
+    MockitoAnnotations.openMocks(this);
   }
 
   @Test
-  public void testGetAllBookings() {
-    // Mock the data expected to be returned by the repository
+  public void testGetAll() {
     List<Booking> bookings = new ArrayList<>();
     bookings.add(new Booking("John Doe", LocalDate.of(2023, 7, 1), LocalDate.of(2023, 7, 5)));
     bookings.add(new Booking("Jane Smith", LocalDate.of(2023, 7, 8), LocalDate.of(2023, 7, 12)));
 
-    // Set up the behavior of the mock repository
     when(bookingRepository.findAll()).thenReturn(bookings);
 
-    // Call the service method
-    //List<Booking> result = bookingService.getAllBookings();
+    List<Booking> result = bookingService.getAll();
 
-    // Verify the result
-    //assertEquals(bookings, result);
+    assertEquals(bookings, result);
+  }
 
-    // Verify the number of times the mock repository was called
-    //verify(bookingRepository, times(1)).findAll();
+  @Test
+  public void testGetAllBookings() {
+    List<Booking> bookings = new ArrayList<>();
+    bookings.add(new Booking("John Doe", LocalDate.of(2023, 7, 1), LocalDate.of(2023, 7, 5)));
+    bookings.add(new Booking("Jane Smith", LocalDate.of(2023, 7, 8), LocalDate.of(2023, 7, 12)));
+
+    when(bookingRepository.findAll()).thenReturn(bookings);
+
+    List<Booking> result = bookingService.getAll();
+
+    assertEquals(bookings, result);
+  }
+
+  @Test
+  public void testGetAllBlocks() {
+    List<Booking> bookings = new ArrayList<>();
+    bookings.add(new Booking("John Doe", LocalDate.of(2023, 7, 1), LocalDate.of(2023, 7, 5)));
+    bookings.add(new Booking("Jane Smith", LocalDate.of(2023, 7, 8), LocalDate.of(2023, 7, 12)));
+
+    when(bookingRepository.findAll()).thenReturn(bookings);
+
+    List<Booking> result = bookingService.getAll();
+
+    assertEquals(bookings, result);
   }
 
   @Test
   public void testGetBookingById() {
-    // Mock the data expected to be returned by the repository
     Booking booking = new Booking("John Doe", LocalDate.of(2023, 7, 1), LocalDate.of(2023, 7, 5));
 
-    // Set up the behavior of the mock repository
     when(bookingRepository.findById(1L)).thenReturn(Optional.of(booking));
 
-    // Call the service method
     Booking result = bookingService.getBookingById(1L);
 
-    // Verify the result
-    //assertEquals(Optional.of(booking), result);
-
-    // Verify the number of times the mock repository was called
-    verify(bookingRepository, times(1)).findById(1L);
+    assertEquals(booking, result);
   }
 
   @Test
-  public void testCreateBooking() {
-    // Create a new booking
+  public void testCreateBookingWithoutOverlappingBooking() {
     Booking newBooking = new Booking("Alice Johnson", LocalDate.of(2023, 7, 15), LocalDate.of(2023, 7, 20));
 
-    // Set up the behavior of the mock repository
-    when(bookingRepository.save(newBooking)).thenReturn(newBooking);
+    List<Booking> bookings = new ArrayList<>();
+    bookings.add(new Booking("John Doe", LocalDate.of(2023, 7, 1), LocalDate.of(2023, 7, 5)));
+    bookings.add(new Booking("Jane Smith", LocalDate.of(2023, 7, 8), LocalDate.of(2023, 7, 12)));
 
-    // Call the service method
-    //Booking createdBooking = bookingService.createOrUpdateBooking(newBooking);
-
-    // Verify the result
-    //assertEquals(newBooking, createdBooking);
-
-    // Verify the number of times the mock repository was called
-    //verify(bookingRepository, times(1)).save(newBooking);
+    assertTrue(bookingValidationService.validateBooking(bookings, newBooking));
   }
 
-  // Other test methods...
+  @Test
+  public void testCreateBookingWithOverlappingBooking() {
+    Booking newBooking = new Booking("Alice Johnson", LocalDate.of(2023, 7, 9), LocalDate.of(2023, 7, 11));
+
+    List<Booking> bookings = new ArrayList<>();
+    bookings.add(new Booking("John Doe", LocalDate.of(2023, 7, 1), LocalDate.of(2023, 7, 5)));
+    bookings.add(new Booking("Jane Smith", LocalDate.of(2023, 7, 8), LocalDate.of(2023, 7, 12)));
+
+    BookingValidationException exception = Assertions.assertThrows(BookingValidationException.class, () -> {
+      bookingValidationService.validateBooking(bookings, newBooking);
+    });
+
+    Assertions.assertEquals("Booking is within the range of another Block or Booking", exception.getMessage());
+  }
+
+  @Test
+  public void testCreateBlockWithOverlappingBlock() {
+    Booking newBooking = new Booking("block", LocalDate.of(2023, 7, 5), LocalDate.of(2023, 7, 20));
+
+    List<Booking> bookings = new ArrayList<>();
+    bookings.add(new Booking("block", LocalDate.of(2023, 7, 1), LocalDate.of(2023, 7, 5)));
+    bookings.add(new Booking("block", LocalDate.of(2023, 7, 8), LocalDate.of(2023, 7, 12)));
+
+    when(bookingRepository.findAll()).thenReturn(bookings);
+
+    bookingService.createOrUpdateBooking(newBooking);
+
+    verify(bookingRepository, times(1)).save(newBooking);
+  }
+
+  @Test
+  public void testCreateBlockWithOverlappingBooking() {
+    Booking newBooking = new Booking("block", LocalDate.of(2023, 7, 15), LocalDate.of(2023, 7, 20));
+
+    List<Booking> bookings = new ArrayList<>();
+    bookings.add(new Booking("John Doe", LocalDate.of(2023, 7, 1), LocalDate.of(2023, 7, 5)));
+    bookings.add(new Booking("Jane Smith", LocalDate.of(2023, 7, 8), LocalDate.of(2023, 7, 12)));
+
+    when(bookingService.getAllBookings()).thenReturn(bookings);
+
+    BookingValidationException exception = Assertions.assertThrows(BookingValidationException.class, () -> {
+      bookingValidationService.validateBlock(bookings, newBooking);
+    });
+
+    Assertions.assertEquals("Booking is within the range of another Block or Booking", exception.getMessage());
+  }
+
+  @Test
+  public void testDeleteBooking() {
+    when(bookingRepository.existsById(1L)).thenReturn(true);
+    bookingService.deleteBooking(1L);
+
+    verify(bookingRepository, times(1)).deleteById(1L);
+  }
 
 }
